ad9f956c4edc4c597415cafb38720a2a556a423f
shaaagri <shaaagri@gmail.com>
2024-09-27 22:20:39 -0700

    Test dynamic page load

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const test = (msg) => { alert(msg); }
+async function fetchHtmlAsText(url) {
+    return await (await fetch(url)).text();
+}
+
+// this is your `load_home() function`
+async function loadHome() {
+    const contentDiv = document.getElementById("root");
+    contentDiv.innerHTML = await fetchHtmlAsText("homepage.html");
+}
+
+loadHome();
