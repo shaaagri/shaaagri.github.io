b3aa3cc8fc4141aadda85b32478d0bf5e610812e
shaaagri <shaaagri@gmail.com>
2024-09-27 22:58:39 -0700

    Implement side nav menu

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-## IAT 355 / A2 Personal Website
+# IAT 355 / A2 Personal Website
 
-# Purpose of the Website
+## Purpose of the Website
 
 This website is intended to be a first draft for a possible future portfolio that would assist job search and personal online representation.
 
-# Technologies Used
+## Technologies Used
 
 The website is written in pure HTML5/CSS3 with a bit of vanilla JavaScript to allow for dynamic web page loading. It permits the navigation bar to stay and change _only_ the content as you navigate (inspired by popular web frameworks such as ReactJS).
 
-# Citations
+## Citations
 
-- https://thenounproject.com/icon/splash-7009016/
-- https://codepen.io/ianfarb/pen/nPaxYR sliding menu effect
-- https://www.w3schools.com/css/css_tooltip.asp
+- https://thenounproject.com/icon/splash-7009016/ - The splash graphic used as the logo
+- https://codepen.io/ianfarb/pen/nPaxYR - The sliding effect in the navgiation menu
+- https://www.w3schools.com/css/css_tooltip.asp - Used for the logo
+- https://stackoverflow.com/questions/17636528/how-do-i-load-an-html-page-in-a-div-using-javascript - Used to implement the dynamic page loading
 
--- main.js
+++ main.js
@@ -1,11 +1,34 @@
+const homePage = "homepage.html";
+const visualizationsPage = "visualizations.html";
+
 async function fetchHtmlAsText(url) {
     return await (await fetch(url)).text();
 }
 
-// this is your `load_home() function`
-async function loadHome() {
-    const contentDiv = document.getElementById("root");
-    contentDiv.innerHTML = await fetchHtmlAsText("homepage.html");
+async function loadPage(page, navMenuItemId) {
+    const rootDiv = document.getElementById("root");
+    rootDiv.innerHTML = await fetchHtmlAsText(page);
+
+    navMenuSetCurrent(navMenuItemId);
 }
 
-loadHome();
+// This function is responsible for the dynamic loading of the pages when user navigates with the side menu
+function navMenuSetCurrent(itemId) {
+    const newCurrentItem = document.getElementById(itemId);
+
+    const itemHome = document.getElementById('nav-main-item-home');
+    const itemVisualizations = document.getElementById('nav-main-item-visualizations');
+
+    itemHome.classList.remove('current');
+    itemVisualizations.classList.remove('current');
+
+    newCurrentItem.classList.add('current');   
+
+    if (itemHome !== newCurrentItem) itemHome.setAttribute('href', "javascript:loadPage(homePage, 'nav-main-item-home')");
+    else itemHome.setAttribute('href', "#");
+
+    if (itemVisualizations !== newCurrentItem) itemVisualizations.setAttribute('href', "javascript:loadPage(visualizationsPage, 'nav-main-item-visualizations')");
+    else itemVisualizations.setAttribute('href', "#");
+}
+
+loadPage(homePage, "nav-main-item-home");
